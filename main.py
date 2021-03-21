de6673c8aceacedff1204d30a475d3ddf5275e2e
Prathviraj <prathvirajsuvarna1999@gmail.com>
2021-03-22 01:02:02 +0530

    Opening browser with command

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 import speech_recognition as sr
 import pyttsx3
+import time
+
+#browswer modules
 import webbrowser as wb
 
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+
+with sr.Microphone () as source: 
+    print ('[ search edureka: search youtube ] ')
+    print ('speak now')
+    audio = r3.listen (source)
+    #time.sleep(2)
+
+    if 'edureka' in r2.recognize_google(audio): 
+
+        r2 = sr.Recognizer()
+        url = "https://www.edureka.co/"
+        #print("1")
+        with sr.Microphone () as source: 
+            print ('search your query') 
+            audio = r2.listen (source)
+            try:
+                get = r2.recognize_google(audio)
+                print(get)
+                wb.get().open_new(url+get) 
+            except sr.UnknownValueError:
+                print("error")
+            except sr.RequestError as e: 
+                print('failed'.format(e))          
+    else:
+        print("Repeat")
+#browswer modules end
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -13,6 +45,7 @@ def speak(text):
     engine.runAndWait()
 
 def take_command():
+    
     try:
         global command
         with sr.Microphone() as source:
@@ -48,5 +81,6 @@ def run_babe():
         speak(txt)
         print(txt)
 
-while(True):
-    run_babe()
+#while(True):
+    #run_babe()
+
