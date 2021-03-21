3c02327b4395c55902fb558ac54e96fb0b69a6a7
Prathviraj <prathvirajsuvarna1999@gmail.com>
2021-03-21 23:05:36 +0530

    Basic replies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
+import webbrowser as wb
 
 
 listener = sr.Recognizer()
@@ -21,10 +22,31 @@ def take_command():
             if 'babe' in command:
                 command = command.replace('babe' , '')
                 print(command)
-                speak(command)
+                #speak(command)
     except:
         pass 
     return command
 
+def run_babe():
+    command = take_command()
+    print(command)
 
-take_command()
+    if 'how are you doing' in command:
+        txt = "I'm doing great, thanks for asking. Anything I can help you with"
+        speak(txt)
+        print(txt)
+    elif 'how are you' in command:
+        txt = "Wonderful, thanks. What can I do for you?"
+        speak(txt)
+        print(txt)
+    elif 'morning' in command:
+        txt = "Very good morning, How may I help you?"
+        speak(txt)
+        print(txt)
+    elif 'night' in command:
+        txt = "Good night, Let me know if i have to set an alarm for you"
+        speak(txt)
+        print(txt)
+
+while(True):
+    run_babe()
