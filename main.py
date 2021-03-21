2be28d7dbe6464356a0af5eb75dfc260d14d89a2
Prathviraj <prathvirajsuvarna1999@gmail.com>
2021-03-21 22:25:11 +0530

    Listen and Speak the same

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import speech_recognition as sr
+import pyttsx3
+
 
 listener = sr.Recognizer()
-try:
-    global command
-    with sr.Microphone() as source:
-        print("listening...")
-        voice= listener.listen(source)
-        command = listener.recognize_google(voice)
-        if 'babe' in command:
-            command = command.replace('babe' , '')
-            print(command)
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
 
-except:
-    pass 
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        global command
+        with sr.Microphone() as source:
+            print("listening...")
+            voice= listener.listen(source)
+            command = listener.recognize_google(voice)
+            if 'babe' in command:
+                command = command.replace('babe' , '')
+                print(command)
+                speak(command)
+    except:
+        pass 
+    return command
+
+
+take_command()
